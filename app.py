3567bcf0afb36914a40c73f32290e87696c93aed
OskarZyg <56176746+OskarZyg@users.noreply.github.com>
2021-07-21 18:20:38 +0100

    Add /version/

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect
+from flask import Flask, jsonify, redirect, request
 from flask_caching import Cache
 
 import data_fetch
@@ -7,9 +7,11 @@ app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
 
+cache_time = 120  # 2 Minutes
+
 
 @app.route('/')
-@cache.cached(timeout=120)  # 2 minute cache should be fine
+@cache.cached(timeout=cache_time)
 def index():
     fetcher = data_fetch.VelocityVersionFetcher()
     return jsonify(
@@ -22,21 +24,33 @@ def index():
 
 
 @app.route('/version/<version>/')
+@cache.cached(timeout=cache_time)
 def specific_version_meta(version: str):
-    return redirect(data_fetch.VelocityDownloadFetcher(version, extension=".module").url, 301)
+    version_object = data_fetch.get_version(version)
+
+    return jsonify({
+        "version": version_object.version,
+        "exists": version_object.real,
+        "snapshot": version_object.snapshot,
+        "download": {
+            "url": version_object.url,
+            "name": version_object.relative_url,
+            "checksum": {
+                "sha512": version_object.velocity_file_sha512,
+                "sha256": version_object.velocity_file_sha256,
+                "sha1": version_object.velocity_file_sha1,
+                "md5": version_object.velocity_file_md5
+            }
+        }
+    })
 
 
-@app.route('/version/<version>/download')
+@app.route('/version/<version>/download/')
+@cache.cached(timeout=cache_time)
 def specific_version_download(version: str):
-    return redirect(data_fetch.VelocityDownloadFetcher(version).url, 301)
+    version_object = data_fetch.get_version(version)
 
-
-@app.route("/snapshot-error/")
-def snapshot_error():
-    return jsonify(
-        error=True,
-        message="SNAPSHOT versions are currently not supported."
-    )
+    return redirect(version_object.url)
 
 
 if __name__ == '__main__':
